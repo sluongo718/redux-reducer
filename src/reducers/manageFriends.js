06afc4e573733c710611cf05e1734133e36d18ae
sluongo718 <sluongo718@yahoo.com>
2021-09-08 20:36:08 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return {friends: [...state.friends, {hometown: action.friend.hometown, id: action.friend.id, name: action.friend.name}]};
+    case "REMOVE_FRIEND":
+      const newState = state.friends.filter(
+        (friend) => friend.id !== action.id
+      );
+      return { friends: newState };
+        default: return state
+    }
+
 }
+
+
+// export function managePresents(state, action){
+//     switch(action.type) {
+//         case "INCREASE":
+//             return {numberOfPresents: state.numberOfPresents + 1 };
+//         default: return state;
+//     }
+
+// }
